e41421f22be3d4a6c644ea521aeadadfc5b566b3
FilipJonsson21 <filipjonsson6611@gmail.com>
2025-05-15 12:53:05 +0200

    Lagt till en klass för databaskoppling InfDB

---
+++ src/databas/Databaskoppling.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package databas;
+
+import gui.LoginFönster;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+
+/**
+ *
+ * @author Filip
+ */
+public class Databaskoppling {
+
+    private static InfDB idb;
+
+    public static void main(String[] args) throws InfException {
+
+        try {
+            idb = new InfDB("SDGSweden", "3306", "dbAdmin2024", "medlemskey");
+            new LoginFönster(idb).setVisible(true);
+            System.out.println("Databasen fungerar");
+
+        } catch (InfException e) {
+            System.out.println("Kunde inte ansluta till databasen" + e.getMessage());
+
+        }
+
+    }
+
+}
