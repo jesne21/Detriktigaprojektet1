57916e1ec7dee311ada99171372f3487647f2d9b
FilipJonsson21 <filipjonsson6611@gmail.com>
2025-05-15 15:18:58 +0200

    Lagt till en klass för databaskoppling InfDB

---
+++ src/ngo2024/NGO2024.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ngo2024;
+
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+/**
+ *
+ * @author Filip
+ */
+public class NGO2024 {
+    
+    private static InfDB idb;
+    
+    public static void main(String [] args){
+        
+        try{
+            idb = new InfDB("SDGSweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
+            new Inloggning().setVisible(true);
+        }catch (InfException ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+}
